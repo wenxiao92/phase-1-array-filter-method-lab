c894af6c6e16fe9e5dc0316ea73ebb4c92bbc2bc
Wen Xiao <wennstyle@gmail.com>
2021-09-15 15:33:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+//function findMatching(names) {
+//    if(names.equalsIgnoreCase('bobby') === true) {
+//        return names;
+//    } else {
+//        return []
+//    }
+//}
+
+function findMatching(array, names) {
+    let newArray = []
+    for (const drivers of array) {
+        if(drivers === names) {
+            return newArray.push(names)
+        } else {
+            return []
+        }
+    }
+}
